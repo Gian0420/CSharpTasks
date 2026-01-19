86fb7199eab06262eeb2fe2b085b0fa46510de61
Gian Cea Reyes <reyesgian035@gmail.com>
2026-01-19 07:11:46 +0000

    Task4

--- ConsoleApp2/obj/Debug/net10.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net10.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2185d15ddc6a599109ea5612617d264543900f09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ca9181c45ab6b0550b8547f03a11e00a6978fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
